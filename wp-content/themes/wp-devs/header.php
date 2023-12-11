0932332f1233240a024064948699a588d9e9d2e0
IagoGoncalves <iago_375@hotmail.com>
2023-12-11 11:27:52 -0300

    modulo 2

---
+++ wp-content/themes/wp-devs/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+<head>
+    <meta charset="<?php bloginfo('charset')?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head() ?>
+</head>
+<body <?php body_class(); ?>>
+    <div id="id" class="site">
+        <header>
+            <section class="top-bar">
+                <div class="logo">a</div>
+                <div class="searchbox">b</div>
+            </section>
+            <section class="menu-area">
+                <nav class="main-menu">c</nav>
+            </section>
+        </header>
