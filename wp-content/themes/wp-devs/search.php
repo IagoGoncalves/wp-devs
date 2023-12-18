d2a0f2519d9a5f0e029f3a7ead2c31cdd6730e75
IagoGoncalves <iago_375@hotmail.com>
2023-12-18 09:35:25 -0300

    template parts

--- wp-content/themes/wp-devs/search.php
+++ wp-content/themes/wp-devs/search.php
@@ -6,21 +6,9 @@
                 <?php 
                     get_search_form();
                     while(have_posts()):
-                        the_post();?>
-                        <header>
-                            <h2><a href="<?php the_permalink()?>"><?php the_title() ?></a></h2>
-                            <?php if('post' == get_post_type()): ?>
-                            <div class="meta-info">
-                                <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
-                                <p>Categories: <?php the_category(' '); ?></p>
-                                <p>Tags: <?php the_tags('', ', '); ?></p>
-                            </div>
-                            <?php endif; ?>
-                        </header>
-                        <div class="content">
-                            <?php the_excerpt(); ?>
-                        </div>
-                    <?php endwhile;
+                        the_post();
+                        get_template_part('parts/content', 'search');
+                    endwhile;
                     the_posts_pagination();
                 ?>
             </div>
